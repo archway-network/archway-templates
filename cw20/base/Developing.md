11bd6293b2164593e62e0ed0eef29435ead406f3
Georges Chouchani <gachouchani@ndu.edu.lb>
2022-07-01 17:16:24 +0300

    simplification of cw20 base

--- cw20/base/Developing.md
+++ cw20/base/Developing.md
@@ -3,12 +3,12 @@
 If you have recently created a contract with this template, you probably could use some
 help on how to build and test the contract, as well as prepare it for production. This
 file attempts to provide a brief overview, assuming you have installed a recent
-version of Rust already (eg. 1.51.0+).
+version of Rust already (eg. 1.58.1+).
 
 ## Prerequisites
 
 Before starting, make sure you have [rustup](https://rustup.rs/) along with a
-recent `rustc` and `cargo` version installed. Currently, we are testing on 1.51.0+.
+recent `rustc` and `cargo` version installed. Currently, we are testing on 1.58.1+.
 
 And you need to have the `wasm32-unknown-unknown` target installed as well.
 
@@ -74,7 +74,15 @@ to run it is this:
 docker run --rm -v "$(pwd)":/code \
   --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
   --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-  cosmwasm/rust-optimizer:0.11.4
+  cosmwasm/rust-optimizer:0.12.4
+```
+
+Or, If you're on an arm64 machine, you should use a docker image built with arm64.
+```sh
+docker run --rm -v "$(pwd)":/code \
+  --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
+  --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
+  cosmwasm/rust-optimizer-arm64:0.12.4
 ```
 
 We must mount the contract code to `/code`. You can use a absolute path instead
--- cw20/base/src/lib.rs
+++ cw20/base/src/lib.rs
@@ -1,35 +1,14 @@
-use schemars::JsonSchema;
-use serde::{Deserialize, Serialize};
 
-use cosmwasm_std::Empty;
+
 use cw2::set_contract_version;
-use cw20::
 
-#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug, Default)]
-pub struct Trait {
-    pub display_type: Option<String>,
-    pub trait_type: String,
-    pub value: String,
-}
+pub use cw20_base::msg::{ExecuteMsg,InstantiateMsg,QueryMsg};
+pub use cw20_base::contract::{instantiate as instantiateCw20, execute as executeCw20, query as queryCw20};
 
-// see: https://ption<String>,
-    pub description: Option<String>,
-    pub name: Option<String>,
-    pub attributes: Option<Vec<Trait>>,
-    pub background_color: Option<String>,
-    pub animation_udocs.opensea.io/docs/metadata-standards
-                   #[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug, Default)]
-                   pub struct Metadata {
-                       pub image: Option<String>,
-                       pub image_data: Option<String>,
-                       pub external_url: Orl: Option<String>,
-    pub youtube_url: Option<String>,
-}
 
-pub type Extension = Option<Metadata>;
 
-pub type Cw721MetadataContract<'a> = cw721_base::Cw<'a, Extension, Empty>;
-pub type ExecuteMsg = cw721_base::ExecuteMsg<Extension>;
+
+
 
 const CONTRACT_NAME: &str = "crates.io:{{project-name}}";
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
@@ -40,6 +19,7 @@ pub mod entry {
 
     use cosmwasm_std::entry_point;
     use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
+    use cw20_base::ContractError;
 
     // This is a simple type to let us handle empty extensions
 
@@ -47,23 +27,12 @@ pub mod entry {
     #[entry_point]
     pub fn instantiate(
         deps: DepsMut,
-        _env: Env,
-        _info: MessageInfo,
+        env: Env,
+        info: MessageInfo,
         msg: InstantiateMsg,
     ) -> StdResult<Response> {
         set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
-
-        let info = ContractInfoResponse {
-            name: msg.name,
-            symbol: msg.symbol,
-        };
-        Cw721MetadataContract::default()
-            .contract_info
-            .save(deps.storage, &info)?;
-        let minter = deps.api.addr_validate(&msg.minter)?;
-        Cw721MetadataContract::default()
-            .minter
-            .save(deps.storage, &minter)?;
+        instantiateCw20(deps, env, info, msg).unwrap();
         Ok(Response::default())
     }
 
@@ -74,57 +43,63 @@ pub mod entry {
         info: MessageInfo,
         msg: ExecuteMsg,
     ) -> Result<Response, ContractError> {
-        Cw721MetadataContract::default().execute(deps, env, info, msg)
+        executeCw20(deps, env, info, msg)
     }
 
     #[entry_point]
     pub fn query(deps: Deps, env: Env, msg: QueryMsg) -> StdResult<Binary> {
-        Cw721MetadataContract::default().query(deps, env, msg)
+        queryCw20(deps, env, msg)
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::entry::instantiate;
+
     use super::*;
 
-    use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
-    use cw721::Cw721Query;
-
+    use cosmwasm_std::{testing::{mock_dependencies, mock_env, mock_info}, Deps};
+    use cw20::{Cw20Coin, TokenInfoResponse};
+    use cosmwasm_std::Uint128;
+    use cw20_base::contract::{query_token_info, query_balance};
     const CREATOR: &str = "creator";
 
+    fn get_balance<T: Into<String>>(deps: Deps, address: T) -> Uint128 {
+        query_balance(deps, address.into()).unwrap().balance
+    }
     #[test]
-    fn use_metadata_extension() {
+    fn basic() {
         let mut deps = mock_dependencies();
-        let contract = Cw721MetadataContract::default();
-
-        let info = mock_info(CREATOR, &[]);
-        let init_msg = InstantiateMsg {
-            name: "SpaceShips".to_string(),
-            symbol: "SPACE".to_string(),
-            minter: CREATOR.to_string(),
+        let amount = Uint128::from(11223344u128);
+        let instantiate_msg = InstantiateMsg {
+            name: "Cash Token".to_string(),
+            symbol: "CASH".to_string(),
+            decimals: 9,
+            initial_balances: vec![Cw20Coin {
+                address: String::from("addr0000"),
+                amount,
+            }],
+            mint: None,
+            marketing: None,
         };
-        contract
-            .instantiate(deps.as_mut(), mock_env(), info.clone(), init_msg)
-            .unwrap();
+        let info = mock_info("creator", &[]);
+        let env = mock_env();
+        let res = instantiate(deps.as_mut(), env, info, instantiate_msg).unwrap();
+        assert_eq!(0, res.messages.len());
 
-        let token_id = "Enterprise";
-        let mint_msg = MintMsg {
-            token_id: token_id.to_string(),
-            owner: "john".to_string(),
-            token_uri: Some("https://starships.example.com/Starship/Enterprise.json".into()),
-            extension: Some(Metadata {
-                description: Some("Spaceship with Warp Drive".into()),
-                name: Some("Starship USS Enterprise".to_string()),
-                ..Metadata::default()
-            }),
-        };
-        let exec_msg = ExecuteMsg::Mint(mint_msg.clone());
-        contract
-            .execute(deps.as_mut(), mock_env(), info, exec_msg)
-            .unwrap();
-
-        let res = contract.nft_info(deps.as_ref(), token_id.into()).unwrap();
-        assert_eq!(res.token_uri, mint_msg.token_uri);
-        assert_eq!(res.extension, mint_msg.extension);
+        assert_eq!(
+            query_token_info(deps.as_ref()).unwrap(),
+            TokenInfoResponse {
+                name: "Cash Token".to_string(),
+                symbol: "CASH".to_string(),
+                decimals: 9,
+                total_supply: amount,
+            }
+        );
+        assert_eq!(
+            get_balance(deps.as_ref(), "addr0000"),
+            Uint128::new(11223344)
+        );
     }
 }
+
