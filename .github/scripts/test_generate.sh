78f1cfae0ffd2475e9f3ac09102939a1f10ab5cb
Georges Chouchani <gachouchani@ndu.edu.lb>
2022-07-08 14:24:16 +0300

    ff

--- .github/scripts/test_generate.sh
+++ .github/scripts/test_generate.sh
@@ -14,9 +14,8 @@ function test-template() {
   echo "#######################################"
   echo "# Testing template $TEMPLATE"
   echo "#######################################"
-  echo "$REPO_ROOT"
   (
-    GIT_BRANCH=$(git -C "$REPO_ROOT" branch --show-current)
+    GIT_BRANCH=$(git -C "$GITHUB_SERVER_URL/$GITHUB_REPOSITORY" branch --show-current)
 
     echo "Generating project from local repository (branch $GIT_BRANCH) ..."
     cargo generate --git "$GITHUB_SERVER_URL/$GITHUB_REPOSITORY" --name "$PROJECT_NAME" --branch "$GIT_BRANCH" "$TEMPLATE"
