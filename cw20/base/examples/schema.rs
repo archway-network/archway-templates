bd742c6dda04a3bbe11af48f2ee121a796230d17
Georges Chouchani <gachouchani@ndu.edu.lb>
2022-07-11 14:58:17 +0300

    change schema to template name

--- cw20/base/examples/schema.rs
+++ cw20/base/examples/schema.rs
@@ -3,7 +3,7 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use cw20_base::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use {{crate_name}}::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
