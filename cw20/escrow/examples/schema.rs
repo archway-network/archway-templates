a5226ae335fa13bf9a0826fd58a9c9ce7c822fd2
Georges Chouchani <gachouchani@ndu.edu.lb>
2022-08-09 16:14:45 +0300

    change import of msgs for escrow

--- cw20/escrow/examples/schema.rs
+++ cw20/escrow/examples/schema.rs
@@ -3,7 +3,7 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use {{crate_name}}::{
+use {{crate_name}}::msg::{
     DetailsResponse, ExecuteMsg, InstantiateMsg, ListResponse, QueryMsg, ReceiveMsg,
 };
 
