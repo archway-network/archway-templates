36060c3cacdc58f5a96765d0acb51847b29e970b
Kayanski <kowalski.kowalskin@gmail.com>
2023-11-08 11:44:22 +0100

    Added cw-orch

--- increment/src/msg.rs
+++ increment/src/msg.rs
@@ -6,6 +6,7 @@ pub struct InstantiateMsg {% raw %}{{% endraw %}{% unless version == "minimal" %
 {% endunless %}}
 
 #[cw_serde]
+#[cfg_attr(feature = "interface", derive(cw_orch::ExecuteFns))]
 pub enum ExecuteMsg {% raw %}{{% endraw %}{% unless version == "minimal" %}
     Increment {},
     Reset { count: i32 },
@@ -13,6 +14,7 @@ pub enum ExecuteMsg {% raw %}{{% endraw %}{% unless version == "minimal" %}
 
 #[cw_serde]
 #[derive(QueryResponses)]
+#[cfg_attr(feature = "interface", derive(cw_orch::QueryFns))]
 pub enum QueryMsg {% raw %}{{% endraw %}{% unless version == "minimal" %}
     // GetCount returns the current count as a json-encoded number
     #[returns(GetCountResponse)]
